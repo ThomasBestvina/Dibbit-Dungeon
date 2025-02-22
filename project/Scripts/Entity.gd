b3a9a35b4a1ad3dd6ca3ee2ed004909e7b6b68ee
Thomas Bestvina <bestvinathomas@gmail.com>
2025-02-21 19:56:42 -0700

    Began main game loop.

--- project/Scripts/Entity.gd
+++ project/Scripts/Entity.gd
@@ -1,4 +1,8 @@
-extends Node2D
+class_name Entity extends Node2D
+
+signal selected(node: Entity)
+
+@export var player: bool = false
 
 @export var max_health: int = 20
 
@@ -9,3 +13,16 @@ extends Node2D
 @export var dice_color_values: Array[Color] = []
 
 @export var items: Array = []
+
+func _ready() -> void:
+	$HealthText.text = health
+	$HealthBar.value = health/max_health*100
+
+func remove_health(val: int):
+	health -= val
+	$HealthText.text = health
+	$HealthBar.value = health/max_health*100
+
+func _on_area_2d_mouse_entered() -> void:
+	if(!player): return
+	emit_signal("selected",self)
